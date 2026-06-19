9b46232553bcfeca6305f59f0cad04d8b7f0f464
Thorix Rover <thorixrover@gmail.com>
2026-06-19 22:06:41 +0900

    Kirim komitmen pertama

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+function App() {
+  return (
+    <>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode="modal"/>
+          <SignUpButton mode="modal"/>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </>
+  )
+}
+
+export default App
