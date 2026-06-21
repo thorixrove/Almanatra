fd275e1a84a88c1aca8e7632c12b40d8741b9ef7
Thorix Rover <thorixrover@gmail.com>
2026-06-22 06:30:24 +0900

    ELPUERO

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,7 @@ import productRouter from "./routes/productRouter"
 import meRouter from "./routes/meRoute"
 import streamRouter from "./routes/streamRouter"
 import chekoutRouter from "./routes/chekoutRouter";
+import { polarWebhookHandler } from "./webhooks/polar";
 
 
 const env = getEnv();
@@ -24,9 +25,9 @@ app.post("/webhooks/clerk", rawjson, (req, res) => {
     void clerkWebhookHandler(req, res);
 })
 
-// app.post("/webhooks/polar", rawjson, (req, res) => {
-//     void polarWebhookHandler(req, res);
-// })
+ app.post("/webhooks/polar", rawjson, (req, res) => {
+     void polarWebhookHandler(req, res);
+ })
 
 
 app.use(express.json());
@@ -42,6 +43,7 @@ app.use("/api/me", meRouter)
 app.use("/api/products", productRouter)
 app.use("/api/stream", streamRouter)
 app.use("/api/checkout", chekoutRouter);
+
 
 
 const publicDir = path.join(process.cwd(),"public")
