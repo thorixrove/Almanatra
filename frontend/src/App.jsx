ae9ce3c3ac357aa762318c8655f9868562aa764d
Thorix Rover <thorixrover@gmail.com>
2026-06-27 08:28:37 +0900

    ELCRAZY

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Show, SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/react'
 import PageLoader from './components/PageLoader'
 import Layout from './components/Layout'
-import { Routes, Route } from "react-router"
+import { Routes, Route, Navigate } from "react-router"
 import HomePage from './Pages/HomePage'
 import CartPage from './Pages/CartPage'
+import OrdersPage from './Pages/OrdersPage'
 
 function App() {
-  const {isLoaded}=useAuth()
+  const {isLoaded, isSignedIn}=useAuth()
 
   if(!isLoaded) return <PageLoader/>
   return (
@@ -14,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/cart" element={<CartPage/>} />
+        <Route path="/orders" element={isSignedIn ? <OrdersPage/> : <Navigate to={"/"} replace/>} 
+        />
       </Routes>
     </Layout>
   )
