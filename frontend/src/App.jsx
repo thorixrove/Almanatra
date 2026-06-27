d94826e75c295e691bd0ce2d97aca8fb8f03ad03
Thorix Rover <thorixrover@gmail.com>
2026-06-28 00:07:05 +0900

    ELMUNTERO

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import { Routes, Route, Navigate } from "react-router"
 import HomePage from './Pages/HomePage'
 import CartPage from './Pages/CartPage'
 import OrdersPage from './Pages/OrdersPage'
+import CheckoutReturnPage from './Pages/CheckoutReturnPage'
+import ProductDetailPage from './Pages/ProductDetailPage'
 
 function App() {
   const {isLoaded, isSignedIn}=useAuth()
@@ -17,6 +19,8 @@ function App() {
         <Route path="/cart" element={<CartPage/>} />
         <Route path="/orders" element={isSignedIn ? <OrdersPage/> : <Navigate to={"/"} replace/>} 
         />
+        <Route path="/checkout/return" element={<CheckoutReturnPage/>} />
+        <Route path="/product/:slug" element={<ProductDetailPage />} />
       </Routes>
     </Layout>
   )
